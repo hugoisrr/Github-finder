36781c08f3ca6dc3fd99cfb34136bc0fc88defb5
hugoisrr <84rami@gmail.com>
2019-10-27 22:54:01 +0100

    refactor UserItem class to function component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </Fragment>
     );
   }
 }
